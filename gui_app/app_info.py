68aa8d2261cefa9b3209b7e7723d3c5cb46f5b01
vincent <vincent51689453@gmail.com>
2020-07-24 02:12:13 +0800

    working on gui

--- gui_app/app_info.py
+++ gui_app/app_info.py
@@ -1,44 +1,199 @@
 #app parameter details
 
 #main interface
+#--------------------------------------------------------------------
 app_main_title = 'CNERC AI Welding Parameters Generator'
 app_main_geometry = '800x600'
 app_main_background = '#292929'
+#--------------------------------------------------------------------
 
 
 #header
+#--------------------------------------------------------------------
 header_style_1 = "Lucida Grande"
 header_style_2 = "Helvetica"
-header_style_3 = "Monaco"
+header_style_3 = "DejaVu Serif"
 
 app_main_header = 'CNERC AI Welding Parameters Generator'
 app_header_fontstyle = header_style_3
-app_header_size = 10
+app_header_size = 15
 app_header_x = 20
-app_header_y = 30
+app_header_y = 20
 app_header_bg = '#292929'
 app_header_text_color = 'white'
+#--------------------------------------------------------------------
+
+
+#input boundary box canvas
+#--------------------------------------------------------------------
+input_canvas_w = 300
+input_canvas_h = 180
+input_canvas_color = '#292929'
+input_canvas_thick = 0
+input_canvas_highlight = 0
+input_canvas_x = 20
+input_canvas_y = 60
+
+input_zone_label = "Manual Input"
+input_label_x = 20
+input_label_y = 10
+input_label_text_color = 'yellow'
+
+#Left
+line1_xmin, line1_xmax, line1_ymin, line1_ymax = 0,0,0,input_canvas_h-1
+#Right
+line2_xmin, line2_xmax, line2_ymin, line2_ymax = input_canvas_w-1,input_canvas_w-1,0,input_canvas_h-1
+#Top
+line3_xmin, line3_xmax, line3_ymin, line3_ymax = 0,input_canvas_w-1,0,0
+#Bottom
+line4_xmin, line4_xmax, line4_ymin, line4_ymax = 0,input_canvas_w-1,input_canvas_h-1,input_canvas_h-1
+#--------------------------------------------------------------------
+
 
 #input current (input1)
+#--------------------------------------------------------------------
 input_current_style_1 = "Lucida Grande"
-input_current_style_2 = "Helvetica"
+input_current_style_2 = "DejaVu Serif"
 
 app_input1_header = 'Input Current (A):'
 app_input1_fontstyle = input_current_style_2
 app_input1_size = 12
+#x,y based on canvas
 app_input1_x = 20
-app_input1_y = 80
+app_input1_y = 50
 app_input1_bg = '#292929'
 app_input1_text_color = 'white'
 app_input1_box_w = 8
-app_input1_box_x = app_input1_x+150
+app_input1_box_x = app_input1_x+180
 app_input1_box_y = app_input1_y
+#--------------------------------------------------------------------
+
+#current speed (input2)
+#--------------------------------------------------------------------
+current_speed_style_1 = "Lucida Grande"
+current_speed_style_2 = "DejaVu Serif"
+
+app_input2_header = 'Speed (In/min):'
+app_input2_fontstyle = current_speed_style_2
+app_input2_size = 12
+#x,y based on canvas
+app_input2_x = 20
+#app_input2_y = app_input1_y + 40
+app_input2_y = 90
+app_input2_bg = '#292929'
+app_input2_text_color = 'white'
+app_input2_box_w = 8
+app_input2_box_x = app_input2_x+180
+app_input2_box_y = app_input2_y
+#--------------------------------------------------------------------
 
 
+#flow rate (input3)
+#--------------------------------------------------------------------
+flow_rate_style_1 = "Lucida Grande"
+flow_rate_style_2 = "DejaVu Serif"
+
+app_input3_header = 'Flow Rate (l/min):'
+app_input3_fontstyle = current_speed_style_2
+app_input3_size = 12
+#x,y based on canvas
+app_input3_x = 20
+#app_input3_y = app_input2_y + 40
+app_input3_y = 130
+app_input3_bg = '#292929'
+app_input3_text_color = 'white'
+app_input3_box_w = 8
+app_input3_box_x = app_input3_x+180
+app_input3_box_y = app_input3_y
+#--------------------------------------------------------------------
+
+#message boundary box canvas
+#--------------------------------------------------------------------
+msg_canvas_w = 440
+msg_canvas_h = 460
+msg_canvas_color = '#292929'
+msg_canvas_thick = 0
+msg_canvas_highlight = 0
+msg_canvas_x = 340
+msg_canvas_y = 60
+
+msg_zone_label = "AI System Message"
+#based on message canvas
+msg_label_x = 20
+msg_label_y = 10
+msg_label_text_color = 'yellow'
+
+#Left
+line1_msg_xmin, line1_msg_xmax, line1_msg_ymin, line1_msg_ymax = 0,0,0,msg_canvas_h-1
+#Right
+line2_msg_xmin, line2_msg_xmax, line2_msg_ymin, line2_msg_ymax = msg_canvas_w-1,msg_canvas_w-1,0,msg_canvas_h-1
+#Top
+line3_msg_xmin, line3_msg_xmax, line3_msg_ymin, line3_msg_ymax = 0,msg_canvas_w-1,0,0
+#Bottom
+line4_msg_xmin, line4_msg_xmax, line4_msg_ymin, line4_msg_ymax = 0,msg_canvas_w-1,msg_canvas_h-1,msg_canvas_h-1
+#--------------------------------------------------------------------
 
 
+#control boundary box canvas
+#--------------------------------------------------------------------
+crtl_canvas_w = 300
+crtl_canvas_h = 260
+crtl_canvas_color = '#292929'
+crtl_canvas_thick = 0
+crtl_canvas_highlight = 0
+crtl_canvas_x = 20
+crtl_canvas_y = 260
+
+crtl_zone_label = "Control Pannel"
+#based on message canvas
+crtl_label_x = 20
+crtl_label_y = 10
+crtl_label_text_color = 'yellow'
+
+#confirm button
+crtl_but_text_size = 10
+crtl_but_conf_text = 'CONFIRM'
+crtl_but_conf_x = 20
+crtl_but_conf_y = 45
+crtl_but_conf_bg = 'DeepSkyBlue2'
+crlt_but_conf_color = 'white'
+
+#Left
+line1_crtl_xmin, line1_crtl_xmax, line1_crtl_ymin, line1_crtl_ymax = 0,0,0,crtl_canvas_h-1
+#Right
+line2_crtl_xmin, line2_crtl_xmax, line2_crtl_ymin, line2_crtl_ymax = crtl_canvas_w-1,crtl_canvas_w-1,0,crtl_canvas_h-1
+#Top
+line3_crtl_xmin, line3_crtl_xmax, line3_crtl_ymin, line3_crtl_ymax = 0,crtl_canvas_w-1,0,0
+#Bottom
+line4_crtl_xmin, line4_crtl_xmax, line4_crtl_ymin, line4_crtl_ymax = 0,crtl_canvas_w-1,crtl_canvas_h-1,crtl_canvas_h-1
+#--------------------------------------------------------------------
 
 
+#output boundary box canvas
+#--------------------------------------------------------------------
+out_canvas_w = 760
+out_canvas_h = 50
+out_canvas_color = '#292929'
+out_canvas_thick = 0
+out_canvas_highlight = 0
+out_canvas_x = 20
+out_canvas_y = 530
+
+out_zone_label = "AI Output"
+#based on message canvas
+out_label_x = 20
+out_label_y = 12
+out_label_text_color = 'yellow'
+
+#Left
+line1_out_xmin, line1_out_xmax, line1_out_ymin, line1_out_ymax = 0,0,0,out_canvas_h-1
+#Right
+line2_out_xmin, line2_out_xmax, line2_out_ymin, line2_out_ymax = out_canvas_w-1,out_canvas_w-1,0,out_canvas_h-1
+#Top
+line3_out_xmin, line3_out_xmax, line3_out_ymin, line3_out_ymax = 0,out_canvas_w-1,0,0
+#Bottom
+line4_out_xmin, line4_out_xmax, line4_out_ymin, line4_out_ymax = 0,out_canvas_w-1,out_canvas_h-1,out_canvas_h-1
+#--------------------------------------------------------------------
 """
 TKinter design reference
 1. http://www.science.smith.edu/dftwiki/index.php/Color_Charts_for_TKinter
--- gui_app/app_main.py
+++ gui_app/app_main.py
@@ -1,10 +1,119 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import app_info
+from PIL import Image,ImageTk
+
+def draw_control_elements(gui_window):
+    fontStyle = tkFont.Font(family=app_info.app_header_fontstyle, size=app_info.crtl_but_text_size)
+    confirm_button = tk.Button(gui_window, text=app_info.crtl_but_conf_text, \
+        bg=app_info.crtl_but_conf_bg,fg=app_info.crlt_but_conf_color,font=fontStyle)
+    confirm_button.place(x=app_info.crtl_but_conf_x,y=app_info.crtl_but_conf_y)
+
+def draw_input_bondary(gui_window):
+    gui_window.create_line(app_info.line1_xmin,app_info.line1_ymin,\
+        app_info.line1_xmax,app_info.line1_ymax,fill='white')       
+    gui_window.create_line(app_info.line2_xmin,app_info.line2_ymin,\
+        app_info.line2_xmax,app_info.line2_ymax,fill='white')
+    gui_window.create_line(app_info.line3_xmin,app_info.line3_ymin,\
+        app_info.line3_xmax,app_info.line3_ymax,fill='white')
+    gui_window.create_line(app_info.line4_xmin,app_info.line4_ymin,\
+        app_info.line4_xmax,app_info.line4_ymax,fill='white')
+
+    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
+    zone_label = tk.Label(gui_window, text=app_info.input_zone_label, font=fontStyle,\
+        bg=app_info.app_input1_bg,fg=app_info.input_label_text_color)
+    zone_label.place(x=app_info.input_label_x,y=app_info.input_label_y)
+
+def draw_message_boundary(gui_window):
+    gui_window.create_line(app_info.line1_msg_xmin,app_info.line1_msg_ymin,\
+        app_info.line1_msg_xmax,app_info.line1_msg_ymax,fill='white')       
+    gui_window.create_line(app_info.line2_msg_xmin,app_info.line2_msg_ymin,\
+        app_info.line2_msg_xmax,app_info.line2_msg_ymax,fill='white')
+    gui_window.create_line(app_info.line3_msg_xmin,app_info.line3_msg_ymin,\
+        app_info.line3_msg_xmax,app_info.line3_msg_ymax,fill='white')
+    gui_window.create_line(app_info.line4_msg_xmin,app_info.line4_msg_ymin,\
+        app_info.line4_msg_xmax,app_info.line4_msg_ymax,fill='white')
+
+    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
+    zone_label = tk.Label(gui_window, text=app_info.msg_zone_label, font=fontStyle,\
+        bg=app_info.app_input1_bg,fg=app_info.msg_label_text_color)
+    zone_label.place(x=app_info.msg_label_x,y=app_info.msg_label_y)
+
+def draw_control_boundary(gui_window): 
+    gui_window.create_line(app_info.line1_crtl_xmin,app_info.line1_crtl_ymin,\
+        app_info.line1_crtl_xmax,app_info.line1_crtl_ymax,fill='white')       
+    gui_window.create_line(app_info.line2_crtl_xmin,app_info.line2_crtl_ymin,\
+        app_info.line2_crtl_xmax,app_info.line2_crtl_ymax,fill='white')
+    gui_window.create_line(app_info.line3_crtl_xmin,app_info.line3_crtl_ymin,\
+        app_info.line3_crtl_xmax,app_info.line3_crtl_ymax,fill='white')
+    gui_window.create_line(app_info.line4_crtl_xmin,app_info.line4_crtl_ymin,\
+        app_info.line4_crtl_xmax,app_info.line4_crtl_ymax,fill='white')
+
+    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
+    zone_label = tk.Label(gui_window, text=app_info.crtl_zone_label, font=fontStyle,\
+        bg=app_info.app_input1_bg,fg=app_info.crtl_label_text_color)
+    zone_label.place(x=app_info.crtl_label_x,y=app_info.crtl_label_y)  
+
+def draw_output_boundary(gui_window):
+    gui_window.create_line(app_info.line1_out_xmin,app_info.line1_out_ymin,\
+        app_info.line1_out_xmax,app_info.line1_out_ymax,fill='white')       
+    gui_window.create_line(app_info.line2_out_xmin,app_info.line2_out_ymin,\
+        app_info.line2_out_xmax,app_info.line2_out_ymax,fill='white')
+    gui_window.create_line(app_info.line3_out_xmin,app_info.line3_out_ymin,\
+        app_info.line3_out_xmax,app_info.line3_out_ymax,fill='white')
+    gui_window.create_line(app_info.line4_out_xmin,app_info.line4_out_ymin,\
+        app_info.line4_out_xmax,app_info.line4_out_ymax,fill='white')
+
+    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
+    zone_label = tk.Label(gui_window, text=app_info.out_zone_label, font=fontStyle,\
+        bg=app_info.app_input1_bg,fg=app_info.out_label_text_color)
+    zone_label.place(x=app_info.out_label_x,y=app_info.out_label_y)       
+
+
+def input_current_init(gui_window):
+    #input parameter 1 = input current
+    #Set input parameter 1 label
+    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
+    input_current_label = tk.Label(gui_window, text=app_info.app_input1_header, font=fontStyle,\
+        bg=app_info.app_input1_bg,fg=app_info.app_input1_text_color)
+    input_current_label.place(x=app_info.app_input1_x,y=app_info.app_input1_y)
+    
+    #Set input parameter 1 input box
+    input_current_entry = tk.Entry(gui_window, width=app_info.app_input1_box_w)
+    input_current_entry.place(x=app_info.app_input1_box_x,y=app_info.app_input1_box_y)
+
+    return input_current_label, input_current_entry
+
+def speed_init(gui_window):
+    #input parameter 2 = current speed
+    fontStyle = tkFont.Font(family=app_info.app_input2_fontstyle, size=app_info.app_input2_size)
+    speed_label = tk.Label(gui_window, text=app_info.app_input2_header, font=fontStyle,\
+        bg=app_info.app_input2_bg,fg=app_info.app_input2_text_color)
+    speed_label.place(x=app_info.app_input2_x,y=app_info.app_input2_y)
+
+    #Set input parameter 2 input box
+    speed_entry = tk.Entry(gui_window, width=app_info.app_input2_box_w)
+    speed_entry.place(x=app_info.app_input2_box_x,y=app_info.app_input2_box_y)   
+
+    return speed_label,speed_entry 
+
+def flow_rate_init(gui_window):
+    #input parameter 3 = flow rate
+    fontStyle = tkFont.Font(family=app_info.app_input3_fontstyle, size=app_info.app_input3_size)
+    flow_rate_label = tk.Label(gui_window, text=app_info.app_input3_header, font=fontStyle,\
+        bg=app_info.app_input3_bg,fg=app_info.app_input3_text_color)
+    flow_rate_label.place(x=app_info.app_input3_x,y=app_info.app_input3_y)
+
+    #Set input parameter 3 input box
+    flow_rate_entry = tk.Entry(gui_window, width=app_info.app_input3_box_w)
+    flow_rate_entry.place(x=app_info.app_input3_box_x,y=app_info.app_input3_box_y)   
+
+    return flow_rate_label,flow_rate_entry 
 
 
 def main_window_init():
     window = tk.Tk()
+
     #Set basic window title, size and background color
     window.title(app_info.app_main_title)
     window.geometry(app_info.app_main_geometry)
@@ -16,24 +125,56 @@ def main_window_init():
         bg=app_info.app_header_bg,fg=app_info.app_header_text_color)
     header_label.place(x=app_info.app_header_x,y=app_info.app_header_y)
 
-
-    #input parater 1 = input current
-    #Set input parameter 1 framework
-    fontStyle = tkFont.Font(family=app_info.app_input1_fontstyle, size=app_info.app_input1_size)
-    input_current_frame = tk.Label(window, text=app_info.app_input1_header, font=fontStyle,\
-        bg=app_info.app_input1_bg,fg=app_info.app_input1_text_color)
-    input_current_frame.place(x=app_info.app_input1_x,y=app_info.app_input1_y)
-    
-    #Set input parameter 1 input box
-    input_current_entry = tk.Entry(window,  width=app_info.app_input1_box_w)
-    input_current_entry.place(x=app_info.app_input1_box_x,y=app_info.app_input1_box_y)
-
-
     return window
 
 
 def main():
+    #Setup root window
     main_window = main_window_init()
+    
+    #Setup canvas for input zone
+    input_area = tk.Canvas(main_window, width=app_info.input_canvas_w, height=app_info.input_canvas_h,\
+        bg = app_info.input_canvas_color, bd = app_info.input_canvas_thick, highlightthickness=app_info.input_canvas_highlight)
+    input_area.place(x=app_info.input_canvas_x,y=app_info.input_canvas_y)
+
+    #Setup different input interfaces on input canvas
+    current_label, current_entry = input_current_init(input_area)
+    speed_label, speed_entry = speed_init(input_area)
+    flow_rate_lable, flow_rate_entry = flow_rate_init(input_area)
+
+    #Draw input boundary boxes
+    draw_input_bondary(input_area)
+
+    #Setup canvas for message zone
+    message_area = tk.Canvas(main_window,width=app_info.msg_canvas_w, height=app_info.msg_canvas_h,\
+        bg = app_info.msg_canvas_color, bd = app_info.msg_canvas_thick, highlightthickness=app_info.msg_canvas_highlight)
+    message_area.place(x=app_info.msg_canvas_x,y=app_info.msg_canvas_y)
+
+    #Draw message boundary boxes
+    draw_message_boundary(message_area)
+
+    #Setup canvas for control
+    control_area = tk.Canvas(main_window,width=app_info.crtl_canvas_w, height=app_info.crtl_canvas_h,\
+        bg = app_info.crtl_canvas_color, bd = app_info.crtl_canvas_thick, highlightthickness=app_info.crtl_canvas_highlight)
+    control_area.place(x=app_info.crtl_canvas_x,y=app_info.crtl_canvas_y)
+
+    #Draw control boundary boxes
+    draw_control_boundary(control_area)
+
+    #Draw control elements
+    draw_control_elements(control_area)
+
+    #Setup canvas for output zone
+    output_area = tk.Canvas(main_window,width=app_info.out_canvas_w, height=app_info.out_canvas_h,\
+        bg = app_info.out_canvas_color, bd = app_info.out_canvas_thick, highlightthickness=app_info.out_canvas_highlight)
+    output_area.place(x=app_info.out_canvas_x,y=app_info.out_canvas_y)    
+
+    #Draw output boundary boxes
+    draw_output_boundary(output_area)
+
+    
+
+    
     main_window.mainloop()
 
 
